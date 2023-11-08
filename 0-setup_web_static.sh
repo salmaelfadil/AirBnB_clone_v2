a60827b1ab58971ca68334f2700bb66a67b286ca
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-11-08 20:20:56 +0200

    .

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,15 +3,12 @@
 
 apt-get update
 apt-get install -y nginx
-
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
-
 printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
