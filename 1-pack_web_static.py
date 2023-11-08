33bc30cd10608f2cffd521dd684df8814bb42970
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-11-08 20:36:31 +0200

    .

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#Fabric script that generates a .tgz archive from the contents
-#of the web_static folder of your AirBnB Clone repo
+# Fabric script that generates a .tgz archive from the contents
+# of the web_static folder of your AirBnB Clone repo
 import os.path
 from datetime import datetime
 from fabric.api import local
