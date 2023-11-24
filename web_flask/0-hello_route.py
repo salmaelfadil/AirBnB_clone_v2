403037736929167563c23fdf6fd7e8da19b19273
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-11-24 22:01:28 +0200

    .

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+"""Starts a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route("/")
+def homepage():
+    """Prints a statement on webpage"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
