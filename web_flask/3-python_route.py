796ee4a4f1a47302e16560e758fb5096c9e45cc0
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-11-24 22:22:19 +0200

    .

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Starts a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def homepage():
+    """Prints a statement on webpage"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb_page():
+    """Prints a statement on webpage"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_is_fun(text):
+    """Prints a statement on webpage"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python/<text>")
+def python_is_cool(text='is cool'):
+    """Prints a statement on webpage"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
