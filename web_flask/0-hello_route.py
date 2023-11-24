0f78030ba410275d8309dc71fb5b879fc3e1b1e0
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-11-24 22:06:16 +0200

    .

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,12 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route("/")
 def homepage():
     """Prints a statement on webpage"""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
