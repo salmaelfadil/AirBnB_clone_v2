feb1021c9a62c528810b54e4fa2411b81f1fdf05
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-11-25 21:58:48 +0200

    .

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,16 +9,19 @@ from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.teardown_appcontext
 def close_db(exc):
     """close the current session"""
     storage.close()
+
 
 @app.route('/states_list')
 def states_list():
     """returns a list of all state objects"""
     states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
